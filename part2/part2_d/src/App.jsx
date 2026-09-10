7380428f6eaba1ce599198a32ad4d0930263b4f6
Jairo Martínez <38862779+JaiMartz@users.noreply.github.com>
2026-09-10 20:28:47 +0200

    recordando 1

---
+++ part2/part2_d/src/App.jsx
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
+import Note from './components/Note'
+
+
+const App = () => {
+  const [notes, setNotes] = useState([])
+  const [newNote, setNewNote] = useState('')
+  const [showAll, setShowAll] = useState(true)
+
+  // is called when the component is mounted, 
+  // and the empty array as the second argument ensures that the effect is only run once, 
+  // when the component is first rendered.
+  useEffect(() => {
+    console.log('effect');
+    axios
+      .get('http://localhost:3001/notes')
+      .then(response => {
+        console.log('promise fulfilled');
+        setNotes(response.data)
+      })
+  }, [])
+
+  console.log('render', notes.length, 'notes');
+
+
+  const addNote = (event) => {
+    event.preventDefault();
+    console.log('button clicker', event.target);
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5
+    }
+
+    axios.post('http://localhost:3001/notes', noteObject)
+      .then(response => {
+        console.log(response);
+        setNotes(notes.concat(response.data))
+        setNewNote('')
+      })
+
+    setNotes(notes.concat(noteObject))
+    setNewNote('')
+  }
+
+  const handleNoteChange = (event) => {
+    console.log(event.target.value);
+    setNewNote(event.target.value)
+  }
+
+  const toggleImportanceOf= (id) => {
+    //console.log(`importance of ${id} needs to be toggled`)
+    const url = `http://localhost:3001/notes/${id}`
+    const note = notes.find( n => n.id === id)
+    const changedNote = {...note, important: !note.important}
+
+    axios.put(url, changedNote).then(response => {
+      setNotes(notes.map(note => note.id !== id ? note : response.data))
+    })
+  }
+
+  const notesToShow = showAll ? notes : notes.filter(note => note.important === true)
+
+  return (
+    <div>
+      <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
+      <ul>
+        {notesToShow.map(note =>
+          <Note 
+            key={note.id} 
+            note={note} 
+            toggleImportance={() => toggleImportanceOf(note.id)} />
+        )}
+      </ul>
+      <form onSubmit={addNote}>
+        <input
+          value={newNote}
+          onChange={handleNoteChange}
+        />
+        <button type="submit">save</button>
+      </form>
+    </div>
+  )
+}
+
+export default App 
